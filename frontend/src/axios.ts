9c491ba71b6c2935cbbddd6b5e42bbd920fd3610
Bagtyyar Rejepov <rejepowbagtyyar05@gmail.com>
2025-04-14 12:06:50 +0500

    added api base url

--- frontend/src/axios.ts
+++ frontend/src/axios.ts
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-    baseURL: 'https://localhost:8000/api'
-})
+    baseURL: `${import.meta.env.VITE_API_BASE_URL}/api`
+});
+
+export default axiosClient;
