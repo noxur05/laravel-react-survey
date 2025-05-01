fd6e96f733b61387fc5a8f6025c8f8124d97577e
Bagtyyar Rejepov <rejepowbagtyyar05@gmail.com>
2025-05-01 12:53:53 +0500

    added survey stuffs

---
+++ app/Http/Controllers/SurveyController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\SurveyStoreRequest;
+use App\Http\Requests\SurveyUpdateRequest;
+use Illuminate\Http\Request;
+
+class SurveyController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+    public function store(SurveyStoreRequest $request)
+    {
+        //
+    }
+
+    public function show(string $id)
+    {
+        //
+    }
+
+    public function update(SurveyUpdateRequest $request, string $id)
+    {
+        //
+    }
+
+    public function destroy(string $id)
+    {
+        //
+    }
+}
