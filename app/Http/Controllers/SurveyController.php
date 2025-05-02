42acaee4f952c2b1d64d0e08618484338c9b77f1
Bagtyyar Rejepov <rejepowbagtyyar05@gmail.com>
2025-05-02 08:44:22 +0500

    bugfix

--- app/Http/Controllers/SurveyController.php
+++ app/Http/Controllers/SurveyController.php
@@ -2,20 +2,31 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\SurveyStoreRequest;
-use App\Http\Requests\SurveyUpdateRequest;
+use App\Http\Requests\StoreSurveyRequest;
+use App\Http\Requests\UpdateSurveyRequest;
+use App\Http\Resources\SurveyResource;
+use App\Models\Survey;
 use Illuminate\Http\Request;
 
 class SurveyController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $user = $request->user();
+        return SurveyResource::collection(
+            Survey::where('user_id', $user->id)
+            ->orderBy('created_at', 'desc')
+            ->paginate(10)
+        );
     }
 
-    public function store(SurveyStoreRequest $request)
+    public function store(StoreSurveyRequest $request)
     {
-        //
+        $data = $request->validated();
+
+        if(isset($data['image'])) {
+
+        }
     }
 
     public function show(string $id)
@@ -23,7 +34,7 @@ class SurveyController extends Controller
         //
     }
 
-    public function update(SurveyUpdateRequest $request, string $id)
+    public function update(UpdateSurveyRequest $request, string $id)
     {
         //
     }
--- app/Http/Requests/UpdateSurveyRequest.php
+++ app/Http/Requests/UpdateSurveyRequest.php
@@ -6,23 +6,25 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class UpdateSurveyRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
     public function authorize(): bool
     {
-        return false;
+        $survey = $this->route('survey'); //TODO
+        if ($this->user()->id !== $survey->user_id) {
+            return false;
+        }
+        return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
     public function rules(): array
     {
         return [
-            //
+            'title' => 'required|string|max:255',
+            'image' => 'nullable|string',
+            'user_id' => 'exists:users,id',
+            'status' => 'required|boolean',
+            'description' => 'nullable|string',
+            'expire_date' => 'nullable|date|after:today',
+            'questions' => 'array'
         ];
     }
 }
